3cac37d93a1d740eab0087e744c0e669076968e8
Dandy Cheng <cheng.dandy@gmail.com>
2024-12-07 21:30:50 +0800

    Fix - No dependencies will fail UT build

--- tests/unit-tests/ut_common.mk
+++ tests/unit-tests/ut_common.mk
@@ -1,5 +1,6 @@
 MAKEFLAGS += --no-print-directory
-VPX_HEADERS = $(sort $(dir $(shell find ../../../src/vulpix/* -name '*.h' -or -name '*.hpp')))
+ROOT_DIR_RELATIVE := ../../..
+VPX_HEADERS = $(sort $(dir $(shell find $(ROOT_DIR_RELATIVE)/src/vulpix/* -name '*.h' -or -name '*.hpp')))
 OMIT_OBJ_FILES += ../../../src/build/obj/vulpix/main.o
 
 setup-env:
@@ -8,17 +9,20 @@ setup-env:
 .SILENT:
 build-deps:
 	echo "Building test dependencies...";
-	$(foreach dep,build-$(DEPENDENCIES),$(MAKE) -C ../../../ $(dep) DEBUG=1;)
-	$(MAKE) -C ../../../ build-vulpix-objs DEBUG=1;
+	if [ -n "$(DEPENDENCIES)" ]; then \
+		$(foreach dep,build-$(DEPENDENCIES),$(MAKE) -C $(ROOT_DIR_RELATIVE)/ $(dep) DEBUG=1;) \
+	fi
+	$(MAKE) -C $(ROOT_DIR_RELATIVE)/ build-vulpix-objs DEBUG=1;
 
 .PHONY:
 test: setup-env build-deps $(OUTPUT_OBJ_FILES)
+	echo "Linking $(TEST_BINARY)..."
 	$(CXX) $(TEST_CXX_FLAGS) -o out/$(TEST_BINARY) $(filter-out $(OMIT_OBJ_FILES),$(call GET_OBJ_FILES,$(OBJ_DIR) $(OBJ_FILE_PATHS))) -lgtest -lgmock
 	$(OBJ_DIR)/../$(TEST_BINARY)
 
 # Clean with the main Makefile to clean dependencies
 clean:
-	$(MAKE) -C ../../.. clean
+	$(MAKE) -C $(ROOT_DIR_RELATIVE) clean
 	rm -rf out
 
 $(OBJ_DIR)/%.o: $(filter-out $(TEST_BINARY).cpp,./%.cpp)
